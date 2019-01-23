90ee7943626d30de6405ee37da0e5bead38c3f99
amedeopro <amedeopro@me.com>
2019-01-23 23:14:14 +0100

    creata base grafica

--- index.php
+++ index.php
@@ -3,8 +3,46 @@
   <head>
     <meta charset="utf-8">
     <title>Google FAQ</title>
+    <link rel="stylesheet" href="css/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
   </head>
   <body>
+
+    <div class="container-header">
+      <div class="only-logo">
+        <div class="logo">
+          <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_74x24dp.png" alt="">
+        </div>
+        <div class="text-logo">
+          <h2>Privacy & Terms</h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-menu">
+      <div class="menu">
+        <div class="menu-left">
+          <ul>
+            <li>Overview</li>
+            <li>Privacy Policy</li>
+            <li>Terms of Service</li>
+            <li>Technologies</li>
+            <li class="blue">FAQ</li>
+          </ul>
+        </div>
+        <div class="menu-right">
+          <p>Google Account</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-faq">
+      <div class="faq">
+
+      </div>
+
+    </div>
+
 
   </body>
 </html>
